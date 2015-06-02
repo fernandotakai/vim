103821489f885f58a4df695f4579c091691e6512
Fernando Takai <fernando.takai@gmail.com>
2015-06-02 11:40:29 -0300

    Fixed installer

--- install.zsh
+++ install.zsh
@@ -3,6 +3,6 @@
 cd ~
 git clone https://github.com/fernandotakai/vim.git .vim
 cd ~
-ln -s .vimrc/vimrc ./.vimrc
+ln -s ~/.vim/vimrc ./.vimrc
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 echo "Done."
